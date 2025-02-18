0900ab65c26bff52f44c378952de0fd7b13d9933
Deepak Singh <deepaky954000@gmail.com>
2025-02-18 11:09:50 +0530

    added form data parser

--- src/configs/server.config.js
+++ src/configs/server.config.js
@@ -1,23 +1,23 @@
-import express from "express";
-import router from "#routes/index";
-import env from "#configs/env";
-import connectDb from "#configs/database";
 import cors from "cors";
+import multer from "multer";
+import express from "express";
+import env from "#configs/env";
+import router from "#routes/index";
+import connectDb from "#configs/database";
 
 const server = express();
 
 await connectDb(env.DB_URI);
 
+server.use(multer().any());
 server.use(express.json());
 server.use("/api", router);
 server.use(
-    cors({
-      origin: "http://localhost:3000", // Allow frontend access
-      methods: "GET,POST,PUT,DELETE",
-      credentials: true, // If sending cookies or auth headers
-    })
-  );
-  
-
+  cors({
+    origin: "http://localhost:3000", // Allow frontend access
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, // If sending cookies or auth headers
+  }),
+);
 
 export default server;
